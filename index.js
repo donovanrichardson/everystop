8593a5d1a1d0a9b2e5a6ca96c292b7efa5d84dca
donovanrichardson <donovanspqa@gmail.com>
2020-10-21 03:26:50 -0400

    ready to deploy

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
-const {client} = require('./config')
+const {client, googleKey, upload} = require('./config')
+const Stop = require('./models/stop')
+const axios = require('axios').default
 
-async function tweet(){
+async function getUploadData(img){
     try{
-        stamp = Date.now()
-        const tweet = await client.post("statuses/update", {
-            status: stamp
-          });
-        console.log(stamp);
+        const uploadData = await upload.post('media/upload', {
+            media_data: img,
+        });
+        return uploadData.media_id_string;
     }catch(e){
         console.log(e);
     }
-    
+
 }
 
-function start(){
+async function tweet(text,img){
+    try{
+        const tweet = await client.post("statuses/update", {
+            status: text,
+            media_ids:[img]
+          });
+    }catch(e){
+        console.log(e);
+    } 
+}
+
+async function processStop(){
+    try{
+        const stop = await Stop.findOne({traversed:{$ne:true}, location_type:{$in:[0,'']}})
+        console.log(stop);
+        stop.traversed = true
+        const meta = await axios.get(`https://maps.googleapis.com/maps/api/streetview/metadata?location=${stop.stop_lat},${stop.stop_lon}&key=${googleKey}`)
+        if(meta.data.status === 'OK'){
+            const photo = await axios.get(`https://maps.googleapis.com/maps/api/streetview?pano=${meta.data.pano_id}&size=640x640&key=${googleKey}`,  {responseType:"arraybuffer"})
+            const photoBin = Buffer.from(photo.data,'binary').toString('base64');
+            console.log(stop.stop_name);
+            const imgId = await getUploadData(photoBin)
+            console.log(imgId);
+            await tweet(stop.stop_name,imgId)
+            
+        }else{ 
+            stop.update({$set:{unavailable:true}})
+        }
+    stop.save()
+    }catch(e){
+        console.log(e)
+    }
+}
+
+async function start(){
     console.log('execution begun');
-    setInterval(tweet, 600000)
+    await processStop()
+    setInterval(processStop, 600000)
 }
 
 start()
